135d2b64689a3150209b296faabdb2f63b04fe0d
HyunwookGil <106861372+HyunwookGil@users.noreply.github.com>
2024-02-16 16:39:47 +0900

    db 설정 test2
    설정 진행중

--- src/main/java/miniPorject/gs_gg/service/test/impl/TestServiceImpl.java
+++ src/main/java/miniPorject/gs_gg/service/test/impl/TestServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.Map;
 @Repository
 public class TestServiceImpl implements TestService {
 
-    @Autowired
+    @Autowired(required = false)
     TestDAO testDAO;
 // private final TestDAO testDAO;
 
